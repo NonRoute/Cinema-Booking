57fc3de2be09988862d373ea05a0f3ab6093723f
NonRoute <chanon.ratn@gmail.com>
2023-02-13 22:58:26 +0700

    Add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router'
+import Login from './Login'
+import Navbar from './components/Navbar'
+import Home from './home'
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</>
+	)
 }
 
 export default App
---
+++ client/src/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+	return (
+		<div>
+			<p>LOGIN</p>
+		</div>
+	)
+}
+
+export default Login
