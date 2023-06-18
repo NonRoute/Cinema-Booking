3182e1e259d20b87ac82a0a472678e49d1a0a96b
NonRoute <chanon.ratn@gmail.com>
2023-06-18 16:38:05 +0700

    Add go to today button

--- client/src/components/DatePicker.jsx
+++ client/src/components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { ArrowPathIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
@@ -17,6 +17,12 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
 		nextDay.setDate(nextDay.getDate() + 1)
 		setSelectedDate(nextDay)
 		localStorage.setItem('selectedDate', nextDay)
+	}
+
+	const handleToday = () => {
+		const today = new Date()
+		setSelectedDate(today)
+		localStorage.setItem('selectedDate', today)
 	}
 
 	const formatDate = (date) => {
@@ -39,6 +45,7 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
 		<div className="flex items-center justify-between gap-2 rounded-md bg-gradient-to-br from-indigo-800 to-blue-700 p-2 font-semibold text-white">
 			{auth.role === 'admin' || !isPast(selectedDate) ? (
 				<button
+					title="Go to yesterdat"
 					className={'rounded hover:bg-gradient-to-br hover:from-indigo-600 hover:to-blue-600'}
 					onClick={handlePrevDay}
 				>
@@ -49,12 +56,22 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
 			)}
 			<h2 className="text-center text-xl sm:text-2xl">{formatDate(selectedDate)}</h2>
 
-			<button
-				className="rounded hover:bg-gradient-to-br hover:from-indigo-600 hover:to-blue-600"
-				onClick={handleNextDay}
-			>
-				<ChevronRightIcon className="h-10 w-10 text-white" />
-			</button>
+			<div className="flex items-center justify-between gap-2">
+				<button
+					title="Go to tomorrow"
+					className="rounded hover:bg-gradient-to-br hover:from-indigo-600 hover:to-blue-600"
+					onClick={handleNextDay}
+				>
+					<ChevronRightIcon className="h-10 w-10 text-white" />
+				</button>
+				<button
+					title="Go to today"
+					className="rounded px-1 hover:bg-gradient-to-br hover:from-indigo-600 hover:to-blue-600"
+					onClick={handleToday}
+				>
+					<ArrowPathIcon className="h-10 w-10 text-white" />
+				</button>
+			</div>
 		</div>
 	)
 }
